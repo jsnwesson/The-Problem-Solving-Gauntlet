5a98c262dc64cddc8e8574185c00b08dbee3496e
Jason Wesson <jsnwesson@gmail.com>
2021-09-13 21:33:14 -0500

    install Enzyme and enzymeAdapter, and create new shallow wrapper for App

--- Trial_2/src/App.test.js
+++ Trial_2/src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App.jsx';
 
-test('renders without error', () => {
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
 });
